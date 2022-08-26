c35d037102381c69cb4592d7b4ec45fdcc11dda8
tawhidul2122 <tawhidul2122@gmail.com>
2022-08-26 22:23:14 +0600

    local storage -->getdata

--- src/cart.js
+++ src/cart.js
@@ -13,9 +13,24 @@ calcuation();
 
 let generateCartItems=()=>{
     if(basket.length!=0){
-        console.log("basket is not empty");
+        // console.log("basket is not empty");
+        return (shoppingCart.innerHTML=basket.map((x)=>{
+            return `
+             <div class="cart-item">
+             <img src="" alt="" />
+             </div>  
+             `;
+        }).join(''));
+    
     }
     else{
+        shoppingCart.innerHTML=``;
+        label.innerHTML=`
+        <h2>Cart is Empty </h2>
+        <a href="./index.html">
+        <button class="home">Back to index.html</button>
+        </a> 
+        `
         console.log("basket si empty");
     }
 };
